7277f765e8f54286284a028463690657a76d3d7e
MdTalha707 <mdsagor0321@gmail.com>
2023-04-12 00:34:22 +0600

    blog questions added

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,63 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getShoppingCart } from "../../Utilities/FakeDb";
-import {FaCreativeCommonsNc,FaMapMarkerAlt,} from "react-icons/fa";
+import { FaCreativeCommonsNc, FaMapMarkerAlt } from "react-icons/fa";
 
 const AppliedJobs = () => {
-  const [storedJob, setStoredJob] = useState([])
-  const [isloading, setIsloading] = useState(false);
-  const [filteredJob, setFilteredJob] = useState()
+  const [storedJob, setStoredJob] = useState([]);
 
+  const [cartJob, setCartjob] = useState([]);
+  const [isloading, setIsloading] = useState(true);
+
+  // fetching here products data
   const loadedData = useLoaderData();
 
+  // fetching here localStorage data .
   useEffect(() => {
+
     const storedJobs = getShoppingCart();
     setStoredJob(storedJobs);
+
+    let cartJobs = [];
+
+    for (const id in storedJob) {
+      const addedJobs = loadedData.find((job) => job.id == id);
+      cartJobs.push(addedJobs);
+    }
+
+    // console.log(cartJobs);
+    setCartjob([...cartJobs]);
+      console.log(cartJob)
   }, [isloading]);
 
-  let cartJobs = [];
+  const handleChange = (e) => {
 
-  for (const id in storedJob) {
-    const addedJobs = loadedData.find((job) => job.id == id);
-    cartJobs.push(addedJobs);
-  }
+    const sortedData = cartJob.filter((job) => job.jobType === e.target.value);
 
-  let filteredArray = [];
-
-  const handleRemoteJobs = () => {
-
-    const check = cartJobs.filter((job) => job.jobType === 'remote');
-    setFilteredJob((filteredArray = [...check]));
+    console.log(sortedData);
+    
+    setCartjob(sortedData);
     setIsloading(!isloading);
   };
-    const handleOnsiteJobs = () => {
-      const check = cartJobs.filter((job) => job.jobType ==='onsite');
-      setFilteredJob((filteredArray = [...check]));
-      setIsloading(!!isloading);
-    };
 
-    console.log(filteredJob)
-    return (
-      <div>
-        <div className="flex items-end flex-col container ">
-          <p className="mt-12 mb-2 pr-16">Filter Jobs By</p>
-          <p>
-            <button
-            
-              onClick={handleRemoteJobs}
-              className="px-3 py-2 bg-slate-200 mr-4 rounded"
-            >
-              remote
-            </button>
-            <button
-              onClick={handleOnsiteJobs}
-              className="px-3 py-2 bg-slate-200 mr-4 rounded"
-            >
-              onsite
-            </button>
-          </p>
-        </div>
-        {/* <div className=" container flex justify-end">
+  return (
+    <div>
+      <div className=" container flex justify-end">
         <select
           onChange={handleChange}
           name="Filter By"
@@ -67,77 +52,44 @@ const AppliedJobs = () => {
           <option value="remote">remote job</option>
           <option value="onsite">onsite job</option>
         </select>
-      </div> */}
-
-        <div className="container grid grid-cols-1 mt-12">
-          {isloading
-            ? filteredJob.map((jobs) => (
-              <div className="flex items-center mb-4">
-                <div className="bg-gray-200 p-4 h-full flex items-center justify-center">
-                  <img className="w-[120px] h-[40px]" src={jobs.companyLogo} />
-                </div>
-                <div className="ml-8">
-                  <p className="text-lg">{jobs.jobTitle}</p>
-                  <p className="text-md">{jobs.companyName}</p>
-                  <div className="my-2">
-                    <a className="border text-xs md:text-base text-cyan-600 border-gray-600 mr-4 p-1 rounded">
-                      {jobs.jobType}
-                    </a>
-                    <a className="border text-xs md:text-base text-cyan-600 border-gray-600  p-1 rounded">
-                      {jobs.fullTimeOrPartTime}
-                    </a>
-                  </div>
-                  <div className="flex gap-4 text-sm my-3">
-                    <p className="flex text-xs md:text-base gap-2 items-center ">
-                      <FaMapMarkerAlt />
-                      {jobs.location}
-                    </p>
-                    <p className="flex text-xs md:text-base gap-2 text-gray-600 items-center">
-                      <FaCreativeCommonsNc />
-                      Salary :{jobs.salaryRange}
-                    </p>
-                  </div>
-                </div>
-                <div className="ml-auto">
-                  <button className="btn-primary"> View Detail</button>
-                </div>
-              </div>
-            ))
-            : cartJobs.map((jobs) => (
-              <div className="flex items-center mb-4">
-                <div className="bg-gray-200 p-4 h-full flex items-center justify-center">
-                  <img className="w-[120px] h-[40px]" src={jobs.companyLogo} />
-                </div>
-                <div className="ml-8">
-                  <p className="text-lg">{jobs.jobTitle}</p>
-                  <p className="text-md">{jobs.companyName}</p>
-                  <div className="my-2">
-                    <a className="border text-xs md:text-base text-cyan-600 border-gray-600 mr-4 p-1 rounded">
-                      {jobs.jobType}
-                    </a>
-                    <a className="border text-xs md:text-base text-cyan-600 border-gray-600  p-1 rounded">
-                      {jobs.fullTimeOrPartTime}
-                    </a>
-                  </div>
-                  <div className="flex gap-4 text-sm my-3">
-                    <p className="flex text-xs md:text-base gap-2 items-center ">
-                      <FaMapMarkerAlt />
-                      {jobs.location}
-                    </p>
-                    <p className="flex text-xs md:text-base gap-2 text-gray-600 items-center">
-                      <FaCreativeCommonsNc />
-                      Salary :{jobs.salaryRange}
-                    </p>
-                  </div>
-                </div>
-                <div className="ml-auto">
-                  <button className="btn-primary"> View Detail</button>
-                </div>
-              </div>
-            ))}
-        </div>
       </div>
-    );
-  };
+
+      <div className="container grid grid-cols-1 mt-12">
+        {cartJob?.map((jobs) => (
+          <div className="flex items-center mb-4">
+            <div className="bg-gray-200 p-4 h-full flex items-center justify-center">
+              <img className="w-[120px] h-[40px]" src={jobs.companyLogo} />
+            </div>
+            <div className="ml-8">
+              <p className="text-lg">{jobs.jobTitle}</p>
+              <p className="text-md">{jobs.companyName}</p>
+              <div className="my-2">
+                <a className="border text-xs md:text-base text-cyan-600 border-gray-600 mr-4 p-1 rounded">
+                  {jobs.jobType}
+                </a>
+                <a className="border text-xs md:text-base text-cyan-600 border-gray-600  p-1 rounded">
+                  {jobs.fullTimeOrPartTime}
+                </a>
+              </div>
+              <div className="flex gap-4 text-sm my-3">
+                <p className="flex text-xs md:text-base gap-2 items-center ">
+                  <FaMapMarkerAlt />
+                  {jobs.location}
+                </p>
+                <p className="flex text-xs md:text-base gap-2 text-gray-600 items-center">
+                  <FaCreativeCommonsNc />
+                  Salary :{jobs.salaryRange}
+                </p>
+              </div>
+            </div>
+            <div className="ml-auto">
+              <button className="btn-primary"> View Detail</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default AppliedJobs;
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,8 +2,111 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
+    <div className="container flex flex-col mt-8">
+      <div className="my-4 p-4 shadow-md bg-pink-200">
+        <p className="font-semibold my-4">When should you use context API ?</p>
+        <p>
+          The Context API in React is a way to share state data across the
+          component tree without the need to pass down props through multiple
+          levels of components. It is a useful tool in certain situations, such
+          as:
+          <br />
+          <br /> When multiple components in your application need to access the
+          same data, but they are not directly related to each other in the
+          component tree.
+          <br /> When you want to avoid prop drilling, which occurs when you
+          pass data down through multiple levels of components just to get it to
+          the component that needs it.
+          <br /> When you want to create a theme or a global state that can be
+          accessed by all components in your application.
+          <br /> When you want to create a provider/consumer pattern to share
+          state between components.
+          <br />
+          <br /> In general, the Context API can be used to simplify data
+          management and reduce the complexity of passing data between
+          components. However, it should be used judiciously and only in
+          situations where it makes sense to do so. If you have a small
+          application with only a few components, the Context API may not be
+          necessary.
+        </p>
+      </div>
+      <div className="my-4 p-4 shadow-md bg-pink-200">
+        <p className="font-semibold my-4">When should you use context API ?</p>
+        <p>
+          In React, a custom hook is a JavaScript function that uses one or more
+          of the built-in React hooks (such as useState, useEffect, useContext,
+          etc.) to encapsulate reusable stateful logic that can be shared across
+          multiple components.
+          <br />
+          <br />
+          Custom hooks allow you to extract common logic from your components
+          and reuse it in a more organized and efficient way. They are called
+          custom hooks because they are not built-in hooks provided by React,
+          but rather custom-made hooks that are created by developers to
+          encapsulate their own logic and functionality.
+          <br />
+          <br />
+          Custom hooks allow you to reuse and share stateful logic across
+          multiple components without having to repeat the same code in each
+          component. They are a powerful tool for creating more maintainable and
+          reusable code in your React applications.
+        </p>
+      </div>
+      <div className="my-4 p-4 shadow-md bg-pink-200">
+        <p className="font-semibold my-4">What is useRef?</p>
+        <p>
+          In React, useRef is a tool that allows you to create a reference to a
+          specific element or value that persists across component renders. It's
+          like giving a name to something that you can use to refer to it later.
+          <br />
+          <br />
+          Sure, let me explain useRef in simpler terms. In React, useRef is a
+          tool that allows you to create a reference to a specific element or
+          value that persists across component renders. It's like giving a name
+          to something that you can use to refer to it later. One common use
+          case for useRef is to access and manipulate DOM elements. For example,
+          you could use useRef to create a reference to an input field, and then
+          use that reference to set the focus on that field when a button is
+          clicked.
+          <br />
+          <br />
+          useRef can also be used to store any value that needs to persist
+          across multiple renders without causing the component to re-render
+          unnecessarily. For example, you could use useRef to store a counter
+          value that needs to persist across multiple renders without triggering
+          a re-render.
+          <br />
+          <br />
+          Overall, useRef is a simple tool that can make it easier to work with
+          mutable values in your React components, without triggering
+          unnecessary re-renders.
+        </p>
+      </div>
+      <div className="my-4 p-4 shadow-md bg-pink-200">
+        <p className="font-semibold my-4">What is useRef?</p>
+        <p>
+          n React, useMemo is a tool that helps you speed up your app by
+          remembering the result of a function so you don't have to calculate it
+          again. It works by caching the result of the function and only
+          recalculating it if the inputs to the function change.
+          <br />
+          <br />
+          For example, if you have a complex function that takes a long time to
+          run, you can use useMemo to remember the result of the function so
+          that you don't have to run it again and again every time your
+          component re-renders. This can save you a lot of time and make your
+          app faster.
+          <br />
+          <br />
+          To use useMemo, you pass it a function and an array of dependencies.
+          The function is the one you want to remember, and the dependencies are
+          the values that the function depends on. If the dependencies don't
+          change, useMemo returns the cached result of the function, otherwise
+          it re-runs the function and returns the new result.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Blog
