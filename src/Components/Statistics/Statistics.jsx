35b6a10a0d834492fb9b8c078d3533e44350584e
MdTalha707 <mdsagor0321@gmail.com>
2023-04-11 15:29:04 +0600

    reCharts js addeed

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { PureComponent } from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
 
 const Statistics = () => {
+
+const marks = [
+  {
+    name: "Assinment-1",
+    mark: 60,
+  },
+  {
+    name: "Assinment-2",
+    mark: 58,
+  },
+  {
+    name: "Assinment-3",
+    mark: 60,
+  },
+  {
+    name: "Assinment-4",
+    mark: 53,
+  },
+  {
+    name: "Assinment-5",
+    mark: 55,
+  },
+  {
+    name: "Assinment-6",
+    mark:58,
+  },
+  {
+    name: "Asinment-7",
+    mark: 60,
+  },
+  ];
+
   return (
-    <div>Statistics</div>
-  )
+    <div className="bg-pink-100 container my-16 p-6">
+
+      <AreaChart
+        width={1100}
+        height={400}
+        data={marks}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="mark" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+    </div>
+  );
 }
 
 export default Statistics
